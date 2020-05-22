cb98ee2625ead5d9e84d37b04d475ed6f3d1fcb1
thebestfella <thisispeter@msn.com>
2020-05-22 13:49:40 -0700

    change clock, add pause functionality

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,14 @@
 let countdown;
+let isPaused = false;
+let pauseSt = 0;
+let pauseEnd = 0;
 
 const timerDisplay = document.querySelector(".display__time-left");
 const timeEndDisplay = document.querySelector(".display__end-time");
 const buttons = document.querySelectorAll(".timer__button");
+const pauseButton = document.querySelector(".pause__button");
+
+pauseButton.addEventListener("click", pause);
 
 buttons.forEach((b) =>
   b.addEventListener("click", function () {
@@ -16,12 +22,31 @@ document.customForm.addEventListener("submit", function (e) {
   timer(parseInt(this.minutes.value) * 60);
 });
 
+function pause() {
+  //already paused
+  console.log("pause called");
+  if (isPaused) {
+    pauseEnd = Date.now();
+    pauseButton.innerHTML = "pause";
+  } else {
+    pauseSt = Date.now();
+    pauseButton.innerHTML = "continue";
+  }
+  isPaused = !isPaused;
+}
+
 function timer(seconds) {
   //clear any intervals in count down
   clearInterval(countdown);
 
   const now = Date.now();
   const then = now + 1000 * seconds;
+
+  //set pause to false
+  isPaused = false;
+  pauseButton.innerHTML = "pause";
+  pauseEnd = 0;
+  pauseSt = 0;
 
   //display timer and comeback
   displayTime(seconds);
@@ -33,25 +58,32 @@ function timer(seconds) {
     timerDisplay.classList.remove("goal");
 
   countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    if (secondsLeft < 0) {
-      new Audio("alarm_long.mp3").play();
-      clearInterval(countdown);
-      return;
-    }
+    if (isPaused) {
+    } else {
+      // console.log(pauseEnd - pauseSt);
+      const secondsLeft = Math.round(
+        (pauseEnd - pauseSt + then - Date.now()) / 1000
+      );
+      if (secondsLeft < 0) {
+        audio.play();
+        clearInterval(countdown);
+        return;
+      }
+      // console.log(isPaused);
 
-    //color text by check seconds left
-    if (secondsLeft < 300 && secondsLeft >= 60) {
-      if (!timerDisplay.classList.contains("almost"))
-        timerDisplay.classList.add("almost");
-    } else if (secondsLeft < 60) {
-      if (timerDisplay.classList.contains("almost"))
-        timerDisplay.classList.remove("almost");
-      if (!timerDisplay.classList.contains("goal"))
-        timerDisplay.classList.add("goal");
-    }
+      //color text by check seconds left
+      if (secondsLeft < 300 && secondsLeft >= 60) {
+        if (!timerDisplay.classList.contains("almost"))
+          timerDisplay.classList.add("almost");
+      } else if (secondsLeft < 60) {
+        if (timerDisplay.classList.contains("almost"))
+          timerDisplay.classList.remove("almost");
+        if (!timerDisplay.classList.contains("goal"))
+          timerDisplay.classList.add("goal");
+      }
 
-    displayTime(secondsLeft);
+      displayTime(secondsLeft);
+    }
   }, 1000);
 }
 
